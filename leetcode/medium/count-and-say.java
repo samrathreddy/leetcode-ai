9356a2c62dc378558d17d9b23cc0d914c4f53ad6
Samrath <102617759+samrathreddy@users.noreply.github.com>
2025-03-26 20:47:02 +0530

    Update solution for : 38. Count and Say - LeetCode

--- leetcode/medium/count-and-say.java
+++ leetcode/medium/count-and-say.java
@@ -47,10 +47,35 @@ Constraints:
 
 Follow up: Could you solve it iteratively?
 */
-// URL: https://leetcode.com/problems/count-and-say/description/
+// URL: https://leetcode.com/problems/count-and-say/submissions/1587044369/
 
-class Solution {
-    public String countAndSay(int n) {
-        st
-    }
+public class Solution {
+    public String countAndSay(int n) {
+        if(n <= 0) return "-1";
+        String result = "1";
+        
+        for(int i = 1; i < n; i ++) {
+            result = build(result);
+        }
+        return result;
+    }
+    
+    private String build(String result) {
+        StringBuilder builder = new StringBuilder
+();
+        int p = 0;
+        while(p < result.length()) {
+            char val = result.charAt(p);
+            int count = 0;
+            
+            while(p < result.length() && 
+              result.charAt(p) == val){
+                p ++;
+                count ++;
+            }
+            builder.append(String.valueOf(count));
+            builder.append(val);
+        }
+        return builder.toString();
+    }
 }
