2975edc3946d5723f590e671ccb1b3a8187c80db
Samrath <102617759+samrathreddy@users.noreply.github.com>
2025-03-26 21:48:54 +0530

    Update solution for : 49. Group Anagrams - LeetCode

--- leetcode/medium/group-anagrams.cpp
+++ leetcode/medium/group-anagrams.cpp
@@ -37,11 +37,21 @@ Constraints:
  * 0 <= strs[i].length <= 100
  * strs[i] consists of lowercase English letters.
 */
-// URL: https://leetcode.com/problems/group-anagrams/
+// URL: https://leetcode.com/problems/group-anagrams/submissions/1587111350/
 
-class Solution {
+class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
-    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            string t = s; 
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+        vector<vector<string>> anagrams;
+        for (auto p : mp) { 
+            anagrams.push_back(p.second);
+        }
+        return anagrams;
+    }
 };
