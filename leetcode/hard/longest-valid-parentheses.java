f7a7edea83241bebd3b319da7742b47bb784275e
Samrath <102617759+samrathreddy@users.noreply.github.com>
2025-03-26 21:03:35 +0530

    Update solution for : 32. Longest Valid Parentheses - LeetCode

--- leetcode/hard/longest-valid-parentheses.java
+++ leetcode/hard/longest-valid-parentheses.java
@@ -35,25 +35,6 @@ Constraints:
  * 0 <= s.length <= 3 * 104
  * s[i] is '(', or ')'.
 */
-// URL: https://leetcode.com/problems/longest-valid-parentheses/submissions/1587055802/
+// URL: https://leetcode.com/problems/longest-valid-parentheses/submissions/1587061138/
 
-public class Solution {
-public int longestValidParentheses(String s) {
-    int max = Integer.MIN_VALUE;
-    s += "x";
-    Stack<Integer> stack = new Stack<>();
-    for(int i = 0; i < s.length(); i++){
-        if(s.charAt(i) == ')' && !stack.empty() && s.charAt(stack.peek())== '(')
-            stack.pop();
-        else{
-            if(stack.empty()){
-                max = Math.max(max, i);
-            }
-            else
-                max = Math.max(max, i-stack.peek()-1);
-            stack.push(i);
-        }
-    }
-    return stack.empty() ? s.length() : max;
-    }
-}
+p
