bbcde422318faafba2df216089eae9cb4ef7986e
Samrath <102617759+samrathreddy@users.noreply.github.com>
2025-03-26 19:53:43 +0530

    Update solution for : 38. Count and Say - LeetCode

--- leetcode/medium/count-and-say.java
+++ leetcode/medium/count-and-say.java
@@ -47,10 +47,10 @@ Constraints:
 
 Follow up: Could you solve it iteratively?
 */
-// URL: https://leetcode.com/problems/count-and-say/
+// URL: https://leetcode.com/problems/count-and-say/description/
 
-class Solution {
-    public String countAndSay(int n) {
-        st
-    }
+class Solution {
+    public String countAndSay(int n) {
+        st
+    }
 }
