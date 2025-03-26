3cc2a4bf4a645637faa14e1423af223f5fb62c7f
Samrath <102617759+samrathreddy@users.noreply.github.com>
2025-03-26 22:04:44 +0530

    Add solution for : 49. Group Anagrams - LeetCode

---
+++ leetcode/medium/group-anagrams.cpp
@@ -0,0 +1,57 @@
+// 49. Group Anagrams - LeetCode
+// Difficulty: Medium
+/*
+Can you solve this real interview question? Group Anagrams - Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+ * There is no string in strs that can be rearranged to form "bat".
+ * The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+ * The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+ 
+
+Constraints:
+
+ * 1 <= strs.length <= 104
+ * 0 <= strs[i].length <= 100
+ * strs[i] consists of lowercase English letters.
+*/
+// URL: https://leetcode.com/problems/group-anagrams/submissions/1587111350/
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            string t = s; 
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+        vector<vector<string>> anagrams;
+        for (auto p : mp) { 
+            anagrams.push_back(p.second);
+        }
+        return anagrams;
+    }
+};
