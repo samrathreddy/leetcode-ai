e1b847947dce30e994be11ca6cc5e152f3c2cd9a
Samrath <102617759+samrathreddy@users.noreply.github.com>
2025-03-27 17:13:43 +0530

    Add solution for : 22. Generate Parentheses - LeetCode

---
+++ leetcode/medium/generate-parentheses.cpp
@@ -0,0 +1,33 @@
+// 22. Generate Parentheses - LeetCode
+// Difficulty: Medium
+/*
+Can you solve this real interview question? Generate Parentheses - Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+
+
+ 
+
+Constraints:
+
+ * 1 <= n <= 8
+*/
+// URL: https://leetcode.com/problems/generate-parentheses/description/
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        
+    }
+};
