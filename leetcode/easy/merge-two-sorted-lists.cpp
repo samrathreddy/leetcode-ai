1d98c11b3e57ca039faa1c32cb874e54ef6884ac
Samrath <102617759+samrathreddy@users.noreply.github.com>
2025-03-27 17:59:29 +0530

    Update solution for : 21. Merge Two Sorted Lists - LeetCode

--- leetcode/easy/merge-two-sorted-lists.cpp
+++ leetcode/easy/merge-two-sorted-lists.cpp
@@ -40,43 +40,29 @@ Constraints:
  * -100 <= Node.val <= 100
  * Both list1 and list2 are sorted in non-decreasing order.
 */
-// URL: https://leetcode.com/problems/merge-two-sorted-lists/submissions/1588080294/
+// URL: https://leetcode.com/problems/merge-two-sorted-lists/description/
 
-class Solution {
+        }
+                swap(l1,l2); // haar baar l1 hi chota hoga
+            }
+                temp -> next = l2;
+            }
+            else {
+                l1 = l1->next;
+                temp = l1;
+            if(l1->val <= l2->val ){
+        while(l1 != NULL && l2 != NULL){
+            
+        ListNode* result = l1;
+        ListNode* temp = NULL;
+            swap(l1,l2);    // haar baar l1 hi chota hoga
+        }
+
+        if(l1->val > l2->val){  // taking L1 is smaller than l2 
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
-
-
-        if(l1->val > l2->val){  // taking L1 is smaller than l2 
-            swap(l1,l2);    // haar baar l1 hi chota hoga
-        }
-        ListNode* result = l1;
-        ListNode* temp = NULL;
-        while(l1 != NULL && l2 != NULL){
-            
-            if(l1->val <= l2->val ){
-                temp = l1;
-                l1 = l1->next;
-            }
-            else {
-                temp -> next = l2;
-                swap(l1,l2); // haar baar l1 hi chota hoga
-            }
-        }
-         while(l1 != NULL){
-            temp->next = l1;
-            temp = l1;
-            l2 = l2->next;
-        }
-        while(l2 != NULL){
-            temp->next = l2;
-            temp = l2;
-            l2 = l2 ->next;
-        }
-       
-        return result;
-    }
-};
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+class Solution {
